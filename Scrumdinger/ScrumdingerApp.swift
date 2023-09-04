d2bbbc2f3a6021b554d8c7913f6eea3c1163b55d
hamihash <hami.hash@yahoo.com>
2023-09-04 18:39:04 +0330

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Hamed Hashemi on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
