f2d9185b632287524e0e9762fef020ff51f49b45
hamihash <hami.hash@yahoo.com>
2023-09-06 04:20:35 +0330

    Add NavigationStacks between mainView, detailsView and meetingsView

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,37 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Hamed Hashemi on 9/6/23.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        NavigationStack {
+            List(scrums) { scrum in
+                NavigationLink(destination: DetailView(scrum: scrum)) {
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.theme.mainColor)
+            }
+            .navigationTitle("Daily Scrums")
+            .toolbar {
+                Button(action: {}) {
+                    Image(systemName: "plus.circle")
+                }
+                .accessibilityLabel("New scrum")
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
--- Scrumdinger/TrailingIconLabelStyle.swift
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -5,4 +5,19 @@
 //  Created by Hamed Hashemi on 9/5/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
